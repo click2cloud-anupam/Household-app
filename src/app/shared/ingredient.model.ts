da5fa72e689428f50e173354e5dac9f7df942bbd
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-05-18 12:04:28 +0530

    created an ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient{
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    //replacement of the above code shown below:
+    constructor(public name: string, public amount: number){
+    }
+}
