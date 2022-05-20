6e12950b05835bf42398d72bddb9563f6b8b1947
Anupam Kumar <anupam.kumar@click2cloud.net>
2022-05-20 17:51:10 +0530

    selection of recipes enabled using data and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , EventEmitter, Output} from '@angular/core';
+// import { EventEmitter } from 'stream';
 import {Recipe} from '../recipes.model';
 
 @Component({
@@ -7,14 +8,19 @@ import {Recipe} from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test desc','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLjeD_t47D4pWirTgGwu4VwCfL5Ekba9cOlw&usqp=CAU'),
-    new Recipe('A Test Recipe', 'This is a test desc','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLjeD_t47D4pWirTgGwu4VwCfL5Ekba9cOlw&usqp=CAU')
+    new Recipe('Another Test Recipe', 'This is a test desc','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLjeD_t47D4pWirTgGwu4VwCfL5Ekba9cOlw&usqp=CAU')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
